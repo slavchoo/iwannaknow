b990f299005ff3b1bf30f348a9e0e94ad3aab7a5
Slava Belokurski <slavchoo2@gmail.com>
2018-03-16 10:16:06 +0300

    add generic DefaultControllerTest unit test

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(1, $crawler->filter('h1:contains("DefaultController")')->count());
+    }
+}
