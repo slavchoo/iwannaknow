6707f0ae5ba176bf99e87c8b84a90b9611c06fec
Slava Belokurski <slavchoo2@gmail.com>
2018-03-22 11:07:04 +0300

    save github user data to the database

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,7 +14,7 @@ class DefaultController extends Controller
      * @Route("/", name="index_page")
      * @Route("/home", name="home_page")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
