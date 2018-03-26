b5fa0ebc73734701b50fe359cf40d428b84b0a78
Slava Belokurski <slavchoo2@gmail.com>
2018-03-26 09:43:44 +0300

    create ReportRetriever class

--- spec/Report/ReportRetrieverSpec.php
+++ spec/Report/ReportRetrieverSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\App\Report;
 
+use App\Document\User;
 use App\Report\ReportRetriever;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -11,5 +12,10 @@ class ReportRetrieverSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType(ReportRetriever::class);
+    }
+
+    function it_is_rerieve_report_by_user(User $user)
+    {
+        $this->retrieve($user);
     }
 }
--- src/Report/ReportRetriever.php
+++ src/Report/ReportRetriever.php
@@ -1,7 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Report;
+
+use App\Document\User;
 
 class ReportRetriever
 {
+    public function retrieve(User $user)
+    {
+        // TODO: write logic here
+    }
 }
