3fcecbe4c9d02569e81953bad3cf946fe28830b1
Alena Belokurskaya <a.belokurskaya@gmail.com>
2018-04-02 22:15:21 +0300

    edit test

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -12,7 +12,7 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertSame(1, $crawler->filter('h1:contains("DefaultController")')->count());
+        $this->assertSame(1, $crawler->filter('a:contains("Connect with")')->count());
     }
 
     /**
