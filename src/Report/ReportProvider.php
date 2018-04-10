7d0ca2248b869cec8357da0923d30380bdd658ab
Slava Belokurski <slavchoo2@gmail.com>
2018-04-10 09:48:46 +0300

    WIP

---
+++ src/Report/ReportProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Report;
+
+use App\Document\User;
+use Github\Api\Search;
+use Github\Client;
+
+class ReportProvider
+{
+    /** @var Client */
+    private $client;
+
+    /** @var User */
+    private $user;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getPullRequestsToReview(): iterable
+    {
+        $this->client->authenticate($this->user->getAccessToken(), null, Client::AUTH_HTTP_TOKEN);
+
+        return $this->getSearchApi()->issues('review-requested:slavchoo is:open');
+    }
+
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    private function getSearchApi(): Search
+    {
+        return $this->client->search();
+    }
+}
