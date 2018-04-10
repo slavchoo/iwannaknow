5a513549e9c8da9fbd34e8b804a6799d303aa8cc
Slava Belokurski <slavchoo2@gmail.com>
2018-04-10 09:59:07 +0300

    use @Template annotation for controller actions

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,20 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DashboardController extends Controller
 {
     /**
      * @Security("has_role('ROLE_USER')")
      * @Route("/home", name="home_page")
+     * @Template()
      */
-    public function index(): Response
+    public function index(): array
     {
-        return $this->render('dashboard/index.html.twig', [
-        ]);
+        return [];
     }
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="index_page")
+     * @Template()
      */
-    public function index(): Response
+    public function index(): array
     {
-        return $this->render('default/index.html.twig', []);
+        return [];
     }
 }
