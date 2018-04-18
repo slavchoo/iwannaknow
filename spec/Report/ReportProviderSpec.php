1ca06f89964f696a465cbd0c9ea916845dedb337
Slava Belokurski <slavchoo2@gmail.com>
2018-04-18 16:10:29 +0300

    [IWK-22] Show list of pull requests awaiting user's review  (#13)
    * WIP
    
    * first report version
    
    * add link to the PR

---
+++ spec/Report/ReportProviderSpec.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\App\Report;
+
+use App\Document\User;
+use App\Report\ReportProvider;
+use Github\Api\Search;
+use Github\Client;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ReportProviderSpec extends ObjectBehavior
+{
+    function let(Client $client)
+    {
+        $this->beConstructedWith($client);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(ReportProvider::class);
+    }
+
+    function it_returns_code_review_request(User $user, Client $client, Search $searchApi)
+    {
+        $user->getAccessToken()->willReturn('gh_token');
+        $client->authenticate('gh_token', null, Client::AUTH_HTTP_TOKEN)->shouldBeCalled();
+
+        $client->search()->shouldBeCalled()->willReturn($searchApi);
+        $searchApi->issues(Argument::type('string'))->willReturn([]);
+
+        $user->getUsername()->shouldBeCalled();
+
+        $this->setUser($user);
+        $this->getPullRequestsToReview();
+    }
+}
